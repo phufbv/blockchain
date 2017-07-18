40658ed72fd16f9242ed4cb23276030a318773e2
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-07-18 23:51:33 +0100

    Refactoring

--- genesis.py
+++
@@ -1,6 +0,0 @@
-import datetime as date
-import block
-
-def create_genesis_block():
-    # Manually construct a block with index zero and arbitrary previous hash
-    return block.Block(0, date.datetime.now(), "Genesis Block", "0")
