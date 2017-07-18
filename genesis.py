84f34e4464e2fe54037ed1346ac2e49c05a1e068
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-07-18 23:42:53 +0100

    Added main script to create blockchain

--- genesis.py
+++ genesis.py
@@ -1,6 +1,6 @@
 import datetime as date
+import block
 
 def create_genesis_block():
-    # Manually construct a block with
-    # index zero and arbitrary previous hash
-    return Block(0, date.datetime.now(), "Genesis Block", "0")
+    # Manually construct a block with index zero and arbitrary previous hash
+    return block.Block(0, date.datetime.now(), "Genesis Block", "0")
